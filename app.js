dea337a4d7d0ffb2c6d7345a84baa993cd3b42e8
kgayed <89406007+kgayed@users.noreply.github.com>
2021-10-05 21:26:27 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var ele=document.body.querySelector(".wrapper");
 //var list =["fire","ice","poison"]
 var dragonDamage=0;
 function dealDamage(damage,attack) {
+    alert("Hello! I am an alert box!!" + attack + " " + damage);
     if (attack==="fire"){
         dragonDamage-=damage;
 
