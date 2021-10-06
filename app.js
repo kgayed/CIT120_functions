141135ed53f64c61704b319e16f31b572cf178e7
kgayed <89406007+kgayed@users.noreply.github.com>
2021-10-06 02:05:05 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-//var ele=document.body.querySelector(".wrapper");
-var ele=document.getElementById("wrapper");
+var ele=document.body.querySelector(".wrapper");
 
 //var list =["fire","ice","poison"]
 var dragonDamage=0;
 function dealDamage(damage,attack) {
-    alert("Hello! I am an alert box!!" + attack + " " + damage);
     if (attack==="fire"){
         dragonDamage-=damage;
 
@@ -13,29 +11,20 @@ function dealDamage(damage,attack) {
         dragonDamage+=damage;
     }
     ele.innerHTML="Dragon Damage: "+ dragonDamage;
+    if (dragonDamage>=10){
+        ele.innerHTML="YOU WON!!!!!"
+    }
 }
-document.getElementById("fire").addEventListener("click",function() {
+
+document.body.querySelector(".fire").addEventListener("click",function() {
     dealDamage(3, "fire");
     ele.innerHTML="Dragon Damage: "+ dragonDamage;
-
 });
-document.getElementById("ice").addEventListener("click",function() {
+document.body.querySelector(".ice").addEventListener("click",function() {
     dealDamage(1,"ice");
 });
 
-document.getElementById("poison").addEventListener("click",function() {
-    dealDamage(4,"poison");
+document.body.querySelector(".poison").addEventListener("click",function() {
+   dealDamage(4,"poison");
 });
-
-//document.body.querySelector(".fire").addEventListener("click",function() {
-  //  dealDamage(3, "fire");
-  //  ele.innerHTML="Dragon Damage: "+ dragonDamage;
-//});
-//document.body.querySelector(".ice").addEventListener("click",function() {
-  //  dealDamage(1,"ice");
-//});
-
-//document.body.querySelector(".poison").addEventListener("click",function() {
-  //  dealDamage(4,"poison");
-//});
 
