c8b6b43fa452d8d6fa364e3736b4c84f71f604e1
kgayed <89406007+kgayed@users.noreply.github.com>
2021-10-08 05:11:06 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,30 +1,36 @@
 var ele=document.body.querySelector(".wrapper");
 
-//var list =["fire","ice","poison"]
-var dragonDamage=0;
-function dealDamage(damage,attack) {
-    if (attack==="fire"){
-        dragonDamage-=damage;
+var dragonDamage=0
 
-    }
-    else if (attack==="ice"){
-        dragonDamage+=damage;
+function dealDamage(damage,attack) {
+    if (attack === "fire") {
+        dragonDamage -=1;
+    } else if (attack === "ice") {
+        dragonDamage += 1;
+    } else if (attack === "poison") {
+        dragonDamage = 4;
     }
     ele.innerHTML="Dragon Damage: "+ dragonDamage;
-    if (dragonDamage>=10){
-        ele.innerHTML="YOU WON!!!!!"
+
+    if (dragonDamage >= 10) {
+        ele.innerHTML = "YOU WON!!!!!"
     }
 }
 
 document.body.querySelector(".fire").addEventListener("click",function() {
-    dealDamage(3, "fire");
-    ele.innerHTML="Dragon Damage: "+ dragonDamage;
+    dealDamage(3,"fire")
+
+    ele.innerHTML = "Dragon Damage: " +dragonDamage;
 });
+
 document.body.querySelector(".ice").addEventListener("click",function() {
     dealDamage(1,"ice");
+
 });
 
 document.body.querySelector(".poison").addEventListener("click",function() {
-   dealDamage(4,"poison");
+    dealDamage(4,"poison");
+
+
 });
 
